df3f9f283e38fea25ce264533e07526b71ed9f05
Steve Whitford <swwhitford@yahoo.com>
2026-06-25 16:24:27 -0400

    changed to May

--- ssdigi_runner.py
+++ ssdigi_runner.py
@@ -6,11 +6,11 @@ from digi_login import dlpoints
 
 if __name__ == "__main__":
     
-    assets = freezer_name_lookup_table.getFreezerIDs()
-    #assets = [445018]
+    #assets = freezer_name_lookup_table.getFreezerIDs()
+    assets = [445018]
 
-    startDate = my_StringtoDatetime("03/1/2026")
-    endDate = my_StringtoDatetime("03/31/2026 23:59:59")
+    startDate = my_StringtoDatetime("05/1/2026")
+    endDate = my_StringtoDatetime("05/31/2026 23:59:59")
 
     eachAssetXL(assets, startDate, endDate)
 
